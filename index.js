a60c40ba0950e658ab52aa64d56298dabd4b548b
Jose <jloai009@fiu.edu>
2021-11-04 07:23:10 -0400

    Node.js/Express back end

---
+++ index.js
@@ -0,0 +1,129 @@
+const { nanoid } = require('nanoid')
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
+
+
+morgan.token('post-info', function (req, res) {
+    if (req.method != 'POST') {
+        return ""
+    }
+    return (JSON.stringify({
+        name: req.body.name,
+        number: req.body.number
+    }))
+})
+
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens['post-info'](req, res)
+  ].join(' ')
+}))
+
+
+
+let persons = [
+    { 
+      "id": nanoid(),
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": nanoid(),
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": nanoid(),
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": nanoid(),
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+const getInfo = () => (
+    `<p><strong>Phonebook</strong> has info for ${persons.length} people.</p>\n
+     <p>${new Date()}</p>`
+)
+
+app.get('/info', (req, res) => {
+    res.send(getInfo())
+})
+
+app.get('/api/persons', (req, res) => {
+    //console.log(persons)
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+ 
+})
+
+app.post('/api/persons/', (req, res) => {
+    const person = req.body
+
+    let personAlreadyExists = false
+    for (let old_person of persons) {
+        if (person.name === old_person.name) {
+            personAlreadyExists = true
+            break
+        }
+    }
+    if (personAlreadyExists) {
+        return res.status(400).json({ 
+            error: 'Name must be unique' 
+        })
+    } else if (!person.name) {
+        return res.status(400).json({ 
+            error: 'Person doesn\'t have a name' 
+        })
+    } else if (!person.number) {
+        return res.status(400).json({ 
+            error: 'Person doesn\'t have a number' 
+        })
+    }
+
+    person.id = nanoid()
+    persons = persons.concat(person)
+    res.json(person)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    persons = persons.filter(person => person.id !== id)
+    console.log(`Deleting person with id ${id}`)
+    res.status(204).end()
+})
+
+
+
+const unknownEndPoint = (request, response) => {
+    response.status(400).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndPoint)
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
